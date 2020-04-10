5c56f75c6f33cb96ac574efc787521f9a695b230
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-10 23:02:31 +0200

    Replace htmled responce with json

--- src/pl/info/czerniak/simplerestapp/api/PersonResource.java
+++ src/pl/info/czerniak/simplerestapp/api/PersonResource.java
@@ -5,23 +5,24 @@ import pl.info.czerniak.simplerestapp.model.PersonDb;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Map;
 
 @Path("/persons")
 public class PersonResource {
 
     @GET
-    public String getPersons(){
-        StringBuilder builder = new StringBuilder();
-        builder.append("<html>");
-        for(Person p : PersonDb.getPersons().values()){
-            builder.append(p.getId());
-            builder.append(" ");
-            builder.append(p.getFirstName());
-            builder.append(" ");
-            builder.append(p.getLastName());
-            builder.append("<br/>");
-        }
-        builder.append("</html>");
-        return builder.toString();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<Long, Person> getPersons(){
+        return PersonDb.getPersons();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person getPerson(@PathParam("id") Long id){
+        return PersonDb.getPersons().get(id);
     }
 }
